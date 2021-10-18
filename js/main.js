e9d4dbfc81b510797bf9c78235b65865d292c90b
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2021-10-18 13:57:45 -0300

    responsive mobile completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 const buttonAboutUs = document.getElementById("button-about-us");
+const buttonContactForm = document.getElementById("button-contact-form");
 const teamSection = document.querySelector("#team-section");
+const formSection = document.querySelector("#form");
 const navbarList = document.querySelector(".navbar-list__container");
 
 //Navbar
@@ -14,7 +16,14 @@ function showNav(){
     navbarList.style.right = "0";
 }
 
+
+
+
+buttonContactForm.addEventListener('click', function () {
+    formSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
+})
 //Button scroll Team
 buttonAboutUs.addEventListener('click', function () {
     teamSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
 })
+
