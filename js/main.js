092630594439538573441fbf82d972a239820d53
tinchoz8426 <76183907+tinchoz8426@users.noreply.github.com>
2021-10-17 23:02:10 -0300

    mobile responsive index

--- js/main.js
+++ js/main.js
@@ -1,11 +1,18 @@
 const buttonAboutUs = document.getElementById("button-about-us");
 const teamSection = document.querySelector("#team-section");
+const navbarList = document.querySelector(".navbar-list__container");
 
 //Navbar
 window.addEventListener('scroll', function () {
     let header = this.document.querySelector("nav");
     header.classList.toggle("sticky", window.scrollY > 10)
 })
+function hideNav(){
+    navbarList.style.right = "-110%";
+}
+function showNav(){
+    navbarList.style.right = "0";
+}
 
 //Button scroll Team
 buttonAboutUs.addEventListener('click', function () {
